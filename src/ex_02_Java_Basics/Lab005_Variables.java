a44de71721c162ecb055f98832bbdf8d60e649c7
Mahathi <grandhimahathi1998@gmail.com>
2025-12-19 13:42:59 +0530

    adding few lines of code

---
+++ src/ex_02_Java_Basics/Lab005_Variables.java
@@ -0,0 +1,29 @@
+package ex_02_Java_Basics;
+
+public class Lab005_Variables {
+    public static void main(String[] args) {
+
+        int a=10;
+        int b=20;
+        int c=a+b;
+        // Sysytem.out.println(c);
+
+        // variable - local, static,instance
+        byte a1 = 10;
+        byte a2 = 10;
+        short a3 = 20;
+        short a4 = 20;
+        char a5 = 'a';
+        char a6 = 'a';
+
+        int _123 = 123;
+        //  System.out.println(_123);
+        //  int enum = 10;
+            int Enum = 10;
+            // short ENum = 10;
+
+        System.out.println(Enum);
+        System.out.printf("a=%d\n", a);
+
+    }
+}
---
+++ src/ex_02_Java_Basics/Lab_004_Comments.java
@@ -0,0 +1,15 @@
+package ex_02_Java_Basics;
+
+/**
+ * Author : Manav
+ * Course : ATB 15x
+ * Learning : Java
+ */
+
+
+public class Lab_004_Comments {
+    public static void main(String[] args) {
+        //this is a comment
+        //this is single line comment
+    }
+}
