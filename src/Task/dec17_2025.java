4291459afba10e463d510595c81f02edbdd0dacd
Mahathi <grandhimahathi1998@gmail.com>
2025-12-19 16:22:47 +0530

    print table of 3 using printf

---
+++ src/Task/dec17_2025.java
@@ -0,0 +1,21 @@
+package Task;
+
+public class dec17_2025 {
+    public static void main(String[] args) {
+
+        // print the table of 3
+        int e = 3;
+        System.out.printf("%d * 1 = %d%n", e, (e));
+        System.out.printf("%d * 2 = %d%n", e, (e * 2));
+        System.out.printf("%d * 3 = %d%n", e, (e * 3));
+        System.out.printf("%d * 4 = %d%n", e, (e * 4));
+        System.out.printf("%d * 5 = %d%n", e, (e * 5));
+        System.out.printf("%d * 6 = %d%n", e, (e * 6));
+        System.out.printf("%d * 7 = %d%n", e, (e * 7));
+        System.out.printf("%d * 8 = %d%n", e, (e * 8));
+        System.out.printf("%d * 9 = %d%n", e, (e * 9));
+        System.out.printf("%d * 10 = %d%n", e, (e * 10));
+        System.out.printf("%d * 11 = %d%n", e, (e * 11));
+        System.out.printf("%d * 12 = %d%n", e, (e * 12));
+    }
+}
